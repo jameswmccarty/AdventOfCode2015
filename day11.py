368667b53ed2c2f44848d278be5e2bc44a573247
jameswmccarty <jameswmccarty@gmail.com>
2019-01-04 10:10:43 -0500

    Add Day 11 file

---
+++ day11.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+"""
+--- Day 11: Corporate Policy ---
+
+Santa's previous password expired, and he needs help choosing a new one.
+
+To help him remember his new password after the old one expires, Santa has devised a method of coming up with a password based on the previous one. Corporate policy dictates that passwords must be exactly eight lowercase letters (for security reasons), so he finds his new password by incrementing his old password string repeatedly until it is valid.
+
+Incrementing is just like counting with numbers: xx, xy, xz, ya, yb, and so on. Increase the rightmost letter one step; if it was z, it wraps around to a, and repeat with the next letter to the left until one doesn't wrap around.
+
+Unfortunately for Santa, a new Security-Elf recently started, and he has imposed some additional password requirements:
+
+    Passwords must include one increasing straight of at least three letters, like abc, bcd, cde, and so on, up to xyz. They cannot skip letters; abd doesn't count.
+    Passwords may not contain the letters i, o, or l, as these letters can be mistaken for other characters and are therefore confusing.
+    Passwords must contain at least two different, non-overlapping pairs of letters, like aa, bb, or zz.
+
+For example:
+
+    hijklmmn meets the first requirement (because it contains the straight hij) but fails the second requirement requirement (because it contains i and l).
+    abbceffg meets the third requirement (because it repeats bb and ff) but fails the first requirement.
+    abbcegjk fails the third requirement, because it only has one double letter (bb).
+    The next password after abcdefgh is abcdffaa.
+    The next password after ghijklmn is ghjaabcc, because you eventually skip all the passwords that start with ghi..., since i is not allowed.
+
+Given Santa's current password (your puzzle input), what should his next password be?
+
+Your puzzle input is cqjxjnds.
+
+--- Part Two ---
+
+Santa's password expired again. What's the next one?
+
+Your puzzle input is still cqjxjnds.
+
+"""
+
+illegals = ['i', 'o', 'l']
+pairs    = { chr(i)+chr(i) for i in range(97,123) }
+
+def straight(word):
+	for i in range(len(word)-2):
+		if word[i+1] == chr(ord(word[i])+1) and word[i+2] == chr(ord(word[i])+2):
+			return True
+	return False
+	
+def doubled(word):
+	for pair1 in pairs:
+		if pair1 in word:
+			for pair2 in pairs.difference({pair1}):
+				if pair2 in word:
+					return True
+	return False
+	
+def inc_password(word, idx):
+	word = list(word)
+	if idx < 0 or idx > len(word)-1:
+		return ''.join(word)
+	nxt = chr((ord(word[idx])-96) % 26 + 97)
+	while nxt in illegals:
+		nxt = chr((ord(nxt)-96) % 26 + 97)
+	word[idx] = nxt
+	if word[idx] == 'a':
+		return inc_password(word, idx-1)
+	else:
+		return ''.join(word)
+	
+def legal(word):
+	if straight(word) and doubled(word):
+		return True
+	return False
+	
+if __name__ == "__main__":
+
+	# Part 1 Solution
+	
+	word = "cqjxjnds"
+	
+	while not legal(word):
+		word = inc_password(word, len(word)-1)
+	print word
+	
+	# Part 2 Solution
+	
+	word = inc_password(word, len(word)-1)
+	while not legal(word):
+		word = inc_password(word, len(word)-1)
+	print word
+	
